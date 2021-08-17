221fe25f75b1e13b10a318148fe707f038dfd35d
sakthi0314 <rsremosakthi@gmail.com>
2021-08-17 04:07:01 -0700

    fixed navbar problem cheaking weather it any probelem

--- src/App.js
+++ src/App.js
@@ -7,31 +7,15 @@ import sal from "sal.js";
 import Contact from "./sections/contact/contact";
 import Footer from "./sections/footer/footer";
 import Projects from "./sections/projects/projects";
-import Loader from "./components/loader/loader";
 
 export class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isLoading: true,
-    };
-  }
-
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({ isLoading: false });
-    }, 4000);
     sal();
   }
 
   render() {
     return (
       <>
-        <Loader
-          style={
-            this.state.isLoading ? {} : { opacity: 0, visibility: "hidden" }
-          }
-        />
         <Navbar />
         <Hero />
         <About />
